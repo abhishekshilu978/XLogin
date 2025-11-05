3c5a8eb28900928c26c21d8cf1fcb53199688b37
abhishekshilu978 <abhishekkumar1087@gmail.com>
2025-11-05 16:00:26 +0530

    Virat Kohli

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
+import React, { useState } from 'react'
+const App = () => {
+  const [user, setUser] = useState({
+    useState: '',
+    password: ''
+  });
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    localStorage.setItem('user', JSON.stringify(user));
+
+    if(!user.username || !user.password){
+      setMessage('Both fields are required');
+      setError(true);
+      console.log("Both fields are required");
+      return;
+    }
+    if(user.username === "user" && user.password === "password"){
+      setMessage(`Welcomme, ${user.username}!`);
+      setError(false)
+    }
+    else{
+      setMessage('Invalid username or password');
+      console.log("Invalid username or password");
+      setError(true);
+    }
+  };
+
+  const handleChange = (e) => {
+    setUser({...user,[e.target.name]: e.target.value})
+  }
   return (
-    <>
+    <div style={{
+      border: "10px solid green", width: "170%", height: "70%", borderRadius: "10px"
+    }}>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 style={{ color: "gold" }}>
+          Login Page
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        {message && (
+          <h3 style={{ color: error ? 'red' : 'green' }}>
+            {message}
+          </h3>
+        )}
       </p>
-    </>
+      <div>
+
+      </div>
+      <div style={{lineHeight: '3', color: "rose", fontSize: "20px"}}>
+        <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit}>
+          <div>
+            <label style={{ fontSize: "30px"}} htmlFor="username">Username: </label>
+            <input style={{ padding: "10px"}} id='username' name='username' type="text" placeholder="username" value={user.username} onChange={handleChange} required />
+          </div>
+          <div>
+          <label style={{ fontSize: "30px"}} htmlFor="password">Password: </label>
+          <input style={{ fontSize: "10px"}} id="password" name="password" type="password" placeholder="password" value={user.password} onChange={handleChange} required />
+          </div>
+          <div>
+            <button style={{ padding: '10px', margin: '10px 40px 0 30%', border:"2px solid red", borderRadius: "5px"}} type="Submit" >
+              Login
+            </button>
+          </div>
+        </form>
+
+      </div>
+    </div>
   )
 }
-
-export default App
+export default App;
